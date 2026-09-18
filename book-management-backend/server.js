ac4663f645cbd1e344dd675375031c6677e73aba
pravalika-banda <pravalikabanda14@gmail.com>
2026-09-18 17:25:15 +0530

    Update backend server configuration

--- book-management-backend/server.js
+++ book-management-backend/server.js
@@ -1,24 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const bookRoutes = require('./routes/book.routes');
-require('dotenv').config();
-
-
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const bookRoutes = require("./routes/book.routes");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
- app.use('/api/books', bookRoutes);
+app.use("/api/books", bookRoutes);
 
 // const PORT = process.env.PORT ?? 5000;
 
-connectDB()
-
+connectDB();
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,"0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
